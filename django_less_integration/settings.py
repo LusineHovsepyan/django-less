c127313575f40d0aab42ef3d6b3128e35408340e
LusineHovsepyan <lhovsep@lhovsep-e6420.internal.synopsys.com>
2016-10-21 22:50:55 +0400

    [in progress...] Compile LESS on server side.
    Previously LESS was comiled on client side by less.js. With this commit
    LESS code is getting comiled on server side with django-static-precompiler.
    
    Now here is remaining to comment code and make some basic frontend.
    
    Signed-off-by: Lusine Hovsepyan <lus.hovsepyan95@gmail.com>

--- django_less_integration/settings.py
+++ django_less_integration/settings.py
@@ -15,6 +15,8 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+# App name
+APP_NAME = 'webapp'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
@@ -38,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'webapp',
+    'static_precompiler',
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -121,3 +124,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, APP_NAME, 'static')
